ee2065583ac2acd1ceff4eba67b5881722ded025
ruchit-p <56946935+ruchit-p@users.noreply.github.com>
2023-10-07 21:08:06 -0400

    edit

--- server.js
+++ server.js
@@ -3,18 +3,27 @@ const mongoose = require('mongoose');
 const app = express();
 const ejs = require('ejs');
 const bodyParser = require("body-parser");
+require('dotenv').config();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect('DBURI', {useNewUrlParser: true}, {useUnifiedTopology: true});
+// Set strictQuery to true to silence the deprecation warning
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.MONGODB_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  w: 'majority',
+  wtimeoutMS: 5000
+});
 
 const moviesSchema = {
     title: String,
     genre: String,
     year: String
-}
+};
 
 const movieModel = mongoose.model("Movie", moviesSchema);
 
@@ -22,20 +31,20 @@ app.get('/', (req, res) => {
     movieModel.find({}, function(err, movies){
         res.render('index', {
             moviesList: movies
-        })
-    })
-})
+        });
+    });
+});
 
 app.post('/', function(req, res){
-    let newMovie = movieModel({
+    let newMovie = new movieModel({
         title: req.body.title,
         genre: req.body.genre,
         year: req.body.year
-    })
+    });
     newMovie.save();
     res.redirect('/');
-})
+});
 
 app.listen('3000', function() {
-    console.log("server is running")
-})
+    console.log("server is running");
+});
